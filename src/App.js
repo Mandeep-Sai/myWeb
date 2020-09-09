ea1538fa4cfe2a6fd4beaf6b633db637344b147a
Nathalie <nathaliemekahal@gmail.com>
2020-09-09 22:21:04 +0200

    title Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
-
+import Title from "./compnents/Title";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Title />
+    </div>
+  );
 }
 
 export default App;
--- src/compnents/Title.jsx
+++ src/compnents/Title.jsx
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
-
+import "./styles/title.css";
+import Typing from "react-typing-animation";
+const textArray = ["hey", "Developer", "Swimmer"];
 class Title extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      textIndex: 0,
+    };
+  }
+  componentDidMount() {
+    this.timeout = setInterval(() => {
+      let currentIdx = this.state.textIndex;
+      this.setState({ textIndex: currentIdx + 1 });
+    }, 1500);
+  }
+
   render() {
-    return <div></div>;
+    let textThatChanges = textArray[this.state.textIndex % textArray.length];
+
+    return (
+      <div className="Title">
+        <div className="Title__titleWrapper">
+          <div className="Title__introduction">hey! I am</div>
+          <div className="Title__mainTitle">Ronaldo Fredrickson</div>
+          <div className="Title__profession">
+            I'm a{" "}
+            <div className="Title__dynamicProfession">
+              <Typing>
+                <span>{textThatChanges}</span>
+                <Typing.Backspace count={20} />
+              </Typing>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 

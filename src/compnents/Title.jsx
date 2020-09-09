3be10a12b8b9c374be170b4b55f432132f8b254e
Nathalie <nathaliemekahal@gmail.com>
2020-09-09 23:47:37 +0200

    typing animation

--- src/compnents/Title.jsx
+++ src/compnents/Title.jsx
@@ -1,35 +1,24 @@
 import React, { Component } from "react";
 import "./styles/title.css";
-import Typing from "react-typing-animation";
-const textArray = ["hey", "Developer", "Swimmer"];
+import Typewriter from "typewriter-effect";
+const textArray = ["Developer", "Swimmer"];
 class Title extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      textIndex: 0,
-    };
-  }
-  componentDidMount() {
-    this.timeout = setInterval(() => {
-      let currentIdx = this.state.textIndex;
-      this.setState({ textIndex: currentIdx + 1 });
-    }, 1500);
-  }
-
   render() {
-    let textThatChanges = textArray[this.state.textIndex % textArray.length];
-
     return (
       <div className="Title">
         <div className="Title__titleWrapper">
           <div className="Title__introduction">hey! I am</div>
           <div className="Title__mainTitle">Ronaldo Fredrickson</div>
           <div className="Title__profession">
-            I'm a{" "}
+            I'm a&nbsp;
             <div className="Title__dynamicProfession">
-              {/* <Typing> */}
-              <span>&nbsp;{textThatChanges}</span>
+              <Typewriter
+                options={{
+                  strings: ["Developer", "Swimmer"],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
               {/* <Typing.Backspace count={20} />
                 <Typing.Delay ms={1000} /> */}
               {/* </Typing> */}

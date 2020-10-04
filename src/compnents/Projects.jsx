0b8e1db614cc2e2f9e25ecda72c4811e8cba94e1
Mandeep Sai <63707885+Mandeep-Sai@users.noreply.github.com>
2020-10-04 17:21:11 +0200

    small stylings

--- src/compnents/Projects.jsx
+++ src/compnents/Projects.jsx
@@ -8,7 +8,42 @@ export default function Projects() {
       <Row>
         <Col>
           <div>
-            <img src="" alt="" />
+            <img
+              src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+              alt=""
+            />
+            <div id="details">
+              <p>Twitter Clone</p>
+              <div>
+                <p>Live Demo</p>
+                <p>Frontend Code</p>
+                <p>Backend Code</p>
+              </div>
+            </div>
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <img
+              src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+              alt=""
+            />
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <img
+              src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+              alt=""
+            />
+          </div>
+        </Col>
+        <Col>
+          <div>
+            <img
+              src="https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fforbestechcouncil%2Ffiles%2F2019%2F01%2Fcanva-photo-editor-8-7.jpg"
+              alt=""
+            />
           </div>
         </Col>
       </Row>
